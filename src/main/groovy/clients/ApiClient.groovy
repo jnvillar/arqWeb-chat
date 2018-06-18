86861d8edd3c610ba7a69ed4dcd5ea26eef54485
jnvillar <juan.villar@mercadolibre.com>
2018-06-17 21:34:23 -0300

    ahora si?

--- src/main/groovy/clients/ApiClient.groovy
+++ src/main/groovy/clients/ApiClient.groovy
@@ -1,21 +1,28 @@
 package clients
 
 import grails.plugins.rest.client.RestBuilder
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 
 
 class ApiClient {
     def rest = new RestBuilder(connectTimeout: 10000, readTimeout: 20000)
 
-    def post(String url, Map form) {
+    def post(String url, Map params) {
+        MultiValueMap form = new LinkedMultiValueMap<>()
+
+        params.each {
+            form.add(it.key, it.value)
+        }
 
         def response = rest.post(url) {
-            contentType "application/json"
-            json(form)
+            accept("application/json")
+            contentType("application/x-www-form-urlencoded")
+            body(form)
         }
 
         println "post response"
         println response.json
-
         response.json
     }
     
