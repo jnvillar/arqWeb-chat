f174f5ff5080ec9212d766b17e883c40edcad169
jnvillar <juan.villar@mercadolibre.com>
2018-06-17 21:51:18 -0300

    ahora?

--- src/main/groovy/clients/ApiClient.groovy
+++ src/main/groovy/clients/ApiClient.groovy
@@ -1,15 +1,21 @@
 package clients
 
 import grails.plugins.rest.client.RestBuilder
+import org.grails.web.json.JSONObject
 
 class ApiClient {
     def rest = new RestBuilder(connectTimeout: 10000, readTimeout: 20000)
 
     def post(String url, Map params) {
+
+        JSONObject json = new JSONObject()
+
+        params.each { json.put(it.key, it.value) }
+
         def response = rest.post(url) {
             accept("application/json")
             contentType("application/json")
-            body(params)
+            body(json)
         }
 
         println "post response"
