174853128eb104f0866118097b91187ca3b8ebee
jnvillar <juan.villar@mercadolibre.com>
2018-06-17 21:25:27 -0300

    el traductor tiene la culpa?2

--- src/main/groovy/clients/ApiClient.groovy
+++ src/main/groovy/clients/ApiClient.groovy
@@ -1,31 +1,16 @@
 package clients
 
-import grails.converters.JSON
 import grails.plugins.rest.client.RestBuilder
-import org.grails.web.json.JSONObject
+
 
 class ApiClient {
     def rest = new RestBuilder(connectTimeout: 10000, readTimeout: 20000)
 
     def post(String url, Map form) {
 
-        JSONObject jsonObject = new JSONObject()
-
-        form.each {
-            jsonObject.put(it.key, it.value)
-        }
-
         def response = rest.post(url) {
             contentType "application/json"
-            json
-                {
-                    to = ["id" : "1", "name":"juan"]
-                    from = ["id":"-1", "name":"fulanito"]
-                    msg = "hola"
-                    attachment = null
-                    sourceApp = "prueba"
-                    targetApp = "prueba"
-                }
+            body(form)
         }
 
         println "post response"
